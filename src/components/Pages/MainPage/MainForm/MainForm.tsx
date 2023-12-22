06758341db2b9665126cf4e43cd6acd2b815f762
Alex Vagulis ( home ) <vagulisal500@gmail.com>
2023-12-22 17:38:12 +0300

    add validation

--- src/components/Pages/MainPage/MainForm/MainForm.tsx
+++ src/components/Pages/MainPage/MainForm/MainForm.tsx
@@ -1,32 +1,40 @@
-import React from "react";
-import {useFormik} from "formik";
+import {Form, Formik} from "formik";
 import styles from "./MainForm.module.scss"
 import {Button, ButtonColor} from "../../../ui/Button";
 import {Input} from "../../../ui/Input";
+import * as Yup from 'yup';
 
 
 const MainForm = () => {
-    const formik = useFormik({
-        initialValues: {numberPhone: "", email: ""},
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    })
+
+    const SignupSchema = Yup.object().shape({
+        phone: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+        email: Yup.string().email('Invalid email').required('Required'),
+    });
 
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <div className={styles.forms}>
-                <div className={styles.formContainer}>
-                    <Input disabled label={'Номер телефона'} placeholder={'+7 999 999-99-99'} type={'tel'} name={'numberPhone'} />
+        <Formik enableReinitialize validateOnChange validationSchema={SignupSchema} initialValues={{phone: "", email: ""}} onSubmit={(values) => {
+            console.log(values);
+        }}>
+            <Form>
+                <div className={styles.forms}>
+                    <div className={styles.formContainer}>
+                        <Input label={'Номер телефона'} placeholder={'+7 999 999-99-99'}
+                               name={'phone'}/>
+                    </div>
+                    <div className={styles.formContainer}>
+                        <Input label={'Email'} placeholder={'webstudio.fractal@example.com'}
+                               name={'email'}/>
+                    </div>
                 </div>
-                <div className={styles.formContainer}>
-                    <Input disabled label={'Email'} placeholder={'webstudio.fractal@example.com'} type={'email'} name={'email'}  />
+                <div className={styles.buttonContainer}>
+                    <Button color={ButtonColor.Blue} children={'Начать'}/>
                 </div>
-            </div>
-            <div className={styles.buttonContainer}>
-                <Button color={ButtonColor.Blue} children={'Начать'}/>
-            </div>
-        </form>
+            </Form>
+        </Formik>
     )
 }
 
--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,22 +1,35 @@
-import React, {FC, InputHTMLAttributes} from "react";
+import {ChangeEventHandler, FC, InputHTMLAttributes, Ref} from "react";
 import styles from './Input.module.scss'
+import {useField} from "formik";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
     label: string;
     placeholder: string;
-    type: string;
     name: string;
 }
 
-export const Input:FC<InputProps> = ({label, placeholder, name, ...rest}) => {
+export const Input:FC<InputProps> = ({label, placeholder, name, onChange, ...rest}) => {
+    const [field, meta, helpers] = useField(name);
+
+    const {error, touched} = meta;
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (
+        event
+    ) => {
+        field.onChange(event);
+    };
+
     return <div className={styles.form}>
         <label className={styles.formLabel} htmlFor={name}>{label}</label>
         <input
+            onChange={handleChange}
             placeholder={placeholder}
             name={name}
+            value={field.value || ''}
             className={styles.formInput}
             {...rest}
         />
+        <div>{touched && error}</div>
     </div>
 }
 
